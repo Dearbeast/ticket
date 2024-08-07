9ba15c589d32e62dd2fdaaa4bcebb27f90703c04
Dearbeast <onaiwuemmanuelide@gmail.com>
2024-08-07 22:49:18 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
                 <div className="row1">
                   <span className="seat">126</span>
                   <span className="seat">4</span>
-                  <span className="seat"> 5</span>
+                  <span className="seat"> 6</span>
                 </div>
               </div>
               <div className="image-content">
@@ -87,56 +87,7 @@ function App() {
                     <h3>TAYLOR SWIFT | THE ERAS TOUR <br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="card-content">
-                <h2 className="name">GENERAL ADMISSION</h2>
-                <button className="btn-apple">
-                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
-                  <span>Add to Apple Wallet</span>
-                </button>
-                <div className="detail">
-                  <a href="">View Barcode</a>
-                  <a href="">Ticket Details</a>
-                </div>
-              </div>
-              <div className="footer-content">
-                <img src="/euZet6wt4nPl.png" alt="" className="love" />
-                <p><i>ticketmaster.verified</i></p>
-              </div>
-            </div>
-            <div className="card swiper-slide">
-              <div className="header-content">
-                <h4>Standard Ticket</h4>
-              </div>
-              <div className="seat-content">
-                <div className="row">
-                  <span className="seat">SEC</span>
-                  <span className="seat">ROW</span>
-                  <span className="seat">SEAT</span>
-                </div>
-                <div className="row1">
-                  <span className="seat">126</span>
-                  <span className="seat">4</span>
-                  <span className="seat"> 6</span>
-                </div>
-              </div>
-              <div className="image-content">
-                <div className="card-img">
-                  <img src="Taylor.jpg" alt="nick" style={{
-                    width: "350px",
-                    height: "350px",
-                    objectFit: "fill"
-                  }} />
-                  <div className="overlay"></div>
-                  <div className="service-desc">
-                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
-
-                    <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
@@ -185,7 +136,7 @@ function App() {
                     <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
@@ -231,59 +182,10 @@ function App() {
                   }} />
                   <div className="overlay"></div>
                   <div className="service-desc">
-                    <h3>TAYLOR SWIFT | THE ERAS TOUR <br /></h3>
-
-                    <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="card-content">
-                <h2 className="name">GENERAL ADMISSION</h2>
-                <button className="btn-apple">
-                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
-                  <span>Add to Apple Wallet</span>
-                </button>
-                <div className="detail">
-                  <a href="">View Barcode</a>
-                  <a href="">Ticket Details</a>
-                </div>
-              </div>
-              <div className="footer-content">
-                <img src="/euZet6wt4nPl.png" alt="" className="love" />
-                <p><i>ticketmaster.verified</i></p>
-              </div>
-            </div>
-            <div className="card swiper-slide">
-              <div className="header-content">
-                <h4>Standard Ticket</h4>
-              </div>
-              <div className="seat-content">
-                <div className="row">
-                  <span className="seat">SEC</span>
-                  <span className="seat">ROW</span>
-                  <span className="seat">SEAT</span>
-                </div>
-                <div className="row1">
-                  <span className="seat">126</span>
-                  <span className="seat">4</span>
-                  <span className="seat"> 5</span>
-                </div>
-              </div>
-              <div className="image-content">
-                <div className="card-img">
-                  <img src="Taylor.jpg" alt="nick" style={{
-                    width: "350px",
-                    height: "350px",
-                    objectFit: "fill"
-                  }} />
-                  <div className="overlay"></div>
-                  <div className="service-desc">
                     <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
@@ -329,10 +231,108 @@ function App() {
                   }} />
                   <div className="overlay"></div>
                   <div className="service-desc">
+                    <h3>TAYLOR SWIFT | THE ERAS TOUR <br /></h3>
+
+                    <p>
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="card-content">
+                <h2 className="name">GENERAL ADMISSION</h2>
+                <button className="btn-apple">
+                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
+                  <span>Add to Apple Wallet</span>
+                </button>
+                <div className="detail">
+                  <a href="">View Barcode</a>
+                  <a href="">Ticket Details</a>
+                </div>
+              </div>
+              <div className="footer-content">
+                <img src="/euZet6wt4nPl.png" alt="" className="love" />
+                <p><i>ticketmaster.verified</i></p>
+              </div>
+            </div>
+            <div className="card swiper-slide">
+              <div className="header-content">
+                <h4>Standard Ticket</h4>
+              </div>
+              <div className="seat-content">
+                <div className="row">
+                  <span className="seat">SEC</span>
+                  <span className="seat">ROW</span>
+                  <span className="seat">SEAT</span>
+                </div>
+                <div className="row1">
+                  <span className="seat">126</span>
+                  <span className="seat">4</span>
+                  <span className="seat"> 7</span>
+                </div>
+              </div>
+              <div className="image-content">
+                <div className="card-img">
+                  <img src="Taylor.jpg" alt="nick" style={{
+                    width: "350px",
+                    height: "350px",
+                    objectFit: "fill"
+                  }} />
+                  <div className="overlay"></div>
+                  <div className="service-desc">
                     <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="card-content">
+                <h2 className="name">GENERAL ADMISSION</h2>
+                <button className="btn-apple">
+                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
+                  <span>Add to Apple Wallet</span>
+                </button>
+                <div className="detail">
+                  <a href="">View Barcode</a>
+                  <a href="">Ticket Details</a>
+                </div>
+              </div>
+              <div className="footer-content">
+                <img src="/euZet6wt4nPl.png" alt="" className="love" />
+                <p><i>ticketmaster.verified</i></p>
+              </div>
+            </div>
+            <div className="card swiper-slide">
+              <div className="header-content">
+                <h4>Standard Ticket</h4>
+              </div>
+              <div className="seat-content">
+                <div className="row">
+                  <span className="seat">SEC</span>
+                  <span className="seat">ROW</span>
+                  <span className="seat">SEAT</span>
+                </div>
+                <div className="row1">
+                  <span className="seat">126</span>
+                  <span className="seat">4</span>
+                  <span className="seat"> 8</span>
+                </div>
+              </div>
+              <div className="image-content">
+                <div className="card-img">
+                  <img src="Taylor.jpg" alt="nick" style={{
+                    width: "350px",
+                    height: "350px",
+                    objectFit: "fill"
+                  }} />
+                  <div className="overlay"></div>
+                  <div className="service-desc">
+                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
+
+                    <p>
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
@@ -366,9 +366,9 @@ function App() {
                   <span className="seat">SEAT</span>
                 </div>
                 <div className="row1">
-                  <span className="seat">117</span>
-                  <span className="seat">6</span>
-                  <span className="seat"> 7</span>
+                  <span className="seat">126</span>
+                  <span className="seat">4</span>
+                  <span className="seat"> 6</span>
                 </div>
               </div>
               <div className="image-content">
@@ -383,105 +383,7 @@ function App() {
                     <h3>TAYLOR SWIFT | THE ERAS TOUR <br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="card-content">
-                <h2 className="name">GENERAL ADMISSION</h2>
-                <button className="btn-apple">
-                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
-                  <span>Add to Apple Wallet</span>
-                </button>
-                <div className="detail">
-                  <a href="">View Barcode</a>
-                  <a href="">Ticket Details</a>
-                </div>
-              </div>
-              <div className="footer-content">
-                <img src="/euZet6wt4nPl.png" alt="" className="love" />
-                <p><i>ticketmaster.verified</i></p>
-              </div>
-            </div>
-            <div className="card swiper-slide">
-              <div className="header-content">
-                <h4>Standard Ticket</h4>
-              </div>
-              <div className="seat-content">
-                <div className="row">
-                  <span className="seat">SEC</span>
-                  <span className="seat">ROW</span>
-                  <span className="seat">SEAT</span>
-                </div>
-                <div className="row1">
-                  <span className="seat">117</span>
-                  <span className="seat">6</span>
-                  <span className="seat"> 8</span>
-                </div>
-              </div>
-              <div className="image-content">
-                <div className="card-img">
-                  <img src="Taylor.jpg" alt="nick" style={{
-                    width: "350px",
-                    height: "350px",
-                    objectFit: "fill"
-                  }} />
-                  <div className="overlay"></div>
-                  <div className="service-desc">
-                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
-
-                    <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="card-content">
-                <h2 className="name">GENERAL ADMISSION</h2>
-                <button className="btn-apple">
-                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
-                  <span>Add to Apple Wallet</span>
-                </button>
-                <div className="detail">
-                  <a href="">View Barcode</a>
-                  <a href="">Ticket Details</a>
-                </div>
-              </div>
-              <div className="footer-content">
-                <img src="/euZet6wt4nPl.png" alt="" className="love" />
-                <p><i>ticketmaster.verified</i></p>
-              </div>
-            </div>
-            <div className="card swiper-slide">
-              <div className="header-content">
-                <h4>Standard Ticket</h4>
-              </div>
-              <div className="seat-content">
-                <div className="row">
-                  <span className="seat">SEC</span>
-                  <span className="seat">ROW</span>
-                  <span className="seat">SEAT</span>
-                </div>
-                <div className="row1">
-                  <span className="seat">117</span>
-                  <span className="seat">6</span>
-                  <span className="seat"> 5</span>
-                </div>
-              </div>
-              <div className="image-content">
-                <div className="card-img">
-                  <img src="Taylor.jpg" alt="nick" style={{
-                    width: "350px",
-                    height: "350px",
-                    objectFit: "fill"
-                  }} />
-                  <div className="overlay"></div>
-                  <div className="service-desc">
-                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
-
-                    <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
@@ -515,7 +417,7 @@ function App() {
                 <div className="row1">
                   <span className="seat">126</span>
                   <span className="seat">4</span>
-                  <span className="seat"> 6</span>
+                  <span className="seat"> 7</span>
                 </div>
               </div>
               <div className="image-content">
@@ -530,7 +432,105 @@ function App() {
                     <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
 
                     <p>
-                      Thu, Aug 15, 16:30 Wembley Stadium London
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="card-content">
+                <h2 className="name">GENERAL ADMISSION</h2>
+                <button className="btn-apple">
+                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
+                  <span>Add to Apple Wallet</span>
+                </button>
+                <div className="detail">
+                  <a href="">View Barcode</a>
+                  <a href="">Ticket Details</a>
+                </div>
+              </div>
+              <div className="footer-content">
+                <img src="/euZet6wt4nPl.png" alt="" className="love" />
+                <p><i>ticketmaster.verified</i></p>
+              </div>
+            </div>
+            <div className="card swiper-slide">
+              <div className="header-content">
+                <h4>Standard Ticket</h4>
+              </div>
+              <div className="seat-content">
+                <div className="row">
+                  <span className="seat">SEC</span>
+                  <span className="seat">ROW</span>
+                  <span className="seat">SEAT</span>
+                </div>
+                <div className="row1">
+                  <span className="seat">126</span>
+                  <span className="seat">4</span>
+                  <span className="seat"> 8</span>
+                </div>
+              </div>
+              <div className="image-content">
+                <div className="card-img">
+                  <img src="Taylor.jpg" alt="nick" style={{
+                    width: "350px",
+                    height: "350px",
+                    objectFit: "fill"
+                  }} />
+                  <div className="overlay"></div>
+                  <div className="service-desc">
+                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
+
+                    <p>
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="card-content">
+                <h2 className="name">GENERAL ADMISSION</h2>
+                <button className="btn-apple">
+                  <img src="/ZDxqK9zTaxuz.png" alt="nick" className="apple" />
+                  <span>Add to Apple Wallet</span>
+                </button>
+                <div className="detail">
+                  <a href="">View Barcode</a>
+                  <a href="">Ticket Details</a>
+                </div>
+              </div>
+              <div className="footer-content">
+                <img src="/euZet6wt4nPl.png" alt="" className="love" />
+                <p><i>ticketmaster.verified</i></p>
+              </div>
+            </div>
+            <div className="card swiper-slide">
+              <div className="header-content">
+                <h4>Standard Ticket</h4>
+              </div>
+              <div className="seat-content">
+                <div className="row">
+                  <span className="seat">SEC</span>
+                  <span className="seat">ROW</span>
+                  <span className="seat">SEAT</span>
+                </div>
+                <div className="row1">
+                  <span className="seat">126</span>
+                  <span className="seat">4</span>
+                  <span className="seat"> 5</span>
+                </div>
+              </div>
+              <div className="image-content">
+                <div className="card-img">
+                  <img src="Taylor.jpg" alt="nick" style={{
+                    width: "350px",
+                    height: "350px",
+                    objectFit: "fill"
+                  }} />
+                  <div className="overlay"></div>
+                  <div className="service-desc">
+                    <h3>TAYLOR SWIFT | THE ERAS TOUR<br /></h3>
+
+                    <p>
+                      Thu, Aug 15, 16:30 Wembley Stadium, London
                     </p>
                   </div>
                 </div>
